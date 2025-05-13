d35b61698897a376a6f7adb147b6a954f620e9bb
Ak <ak.ou.akela@gmail.com>
2025-05-13 15:32:14 +0200

    test liste

--- script/script.js
+++ script/script.js
@@ -1,6 +1,5 @@
-
-const ctx = document.getElementById('myChart');
-new Chart(ctx, {
+const ctx1 = document.getElementById('myChart');
+new Chart(ctx1, {
   type: 'bar',
   data: {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -11,20 +10,47 @@ new Chart(ctx, {
     }]
   },
   options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Top 10 des artistes (Nombres de morceaux)',
+      },
+      legend: {
+        display: false // Désactiver la légende
+      }
+    },
     scales: {
-      y: {
+      x: {
         beginAtZero: true
       }
     }
   }
 });
 
-
-
-const config = {
-    type: 'bar',
-    data,
-    options: {
-      indexAxis: 'y',
+const ctx2 = document.getElementById('myChart2');
+new Chart(ctx2, {
+  type: 'pie',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        position: 'right'
+      },
+      title: {
+        display: true,
+        text: 'Distribution des genres musicaux'
+      }
     }
-  };
+  }
+});
